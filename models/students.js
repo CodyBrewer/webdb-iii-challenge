bcc8145edb4b91277456a95b97a43ae496c793ad
Cody Brewer <brewer.cody.m@gmail.com>
2019-05-27 14:15:39 -0600

    model done for students

---
+++ models/students.js
@@ -0,0 +1,35 @@
+const db = require('../data/dbConfig');
+
+const find = () => {
+  return db('students');
+};
+
+const findById = id => {
+  return db('students')
+    .where({ id })
+    .first();
+};
+
+const add = cohort => {
+  return db('students').insert(student);
+};
+
+const remove = id => {
+  return db('students')
+    .where({ id })
+    .delete();
+};
+
+const update = (id, changes) => {
+  return db('students')
+    .where({ id })
+    .update(changes);
+};
+
+module.exports = {
+  find,
+  findById,
+  add,
+  remove,
+  update
+};
